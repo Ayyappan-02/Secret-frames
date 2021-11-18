1a9cfc3d2971f0a6cb0f3ca3a58d3ba5fed947dc
BastaAditya <54901460+BastaAditya@users.noreply.github.com>
2021-11-18 12:29:07 +0530

    stego

--- steg/encrypt.py
+++ steg/encrypt.py
@@ -1,7 +1,8 @@
 import cv2
 import random
+from stegano import lsb
 
-from steg.utils import _split_frames, _lsb_encode, _embed_frame, _wrap_up, _lsb_decode
+from steg.utils import _split_frames, _embed_frame, _wrap_up
 
 def steg_photo(path1, path2):
 	steg_key = "frames="
@@ -9,19 +10,20 @@ def steg_photo(path1, path2):
 	vidObj = cv2.VideoCapture(path1)
 	steg_img = cv2.imread(path2)
 	frame_array, size = _split_frames(vidObj)
+	n  = len(frame_array)
 	steg_img = cv2.resize(steg_img, (size[0]//2, size[1]//2))
-	frame_array, frames = _embed_frame(frame_array, steg_img, [])
+	frame_array, frames = _embed_frame(frame_array, steg_img, [n//2])
 	for frame in frames:
 		steg_key += str(frame) + ","
 	#encrypt before using the string directly
-	print(steg_key)
-	frame_array[0] = _lsb_encode(frame_array[0], steg_key)
-	print(_lsb_decode(frame_array[0]))
+	cv2.imwrite("tmp.png", frame_array[n//2])
+	secret = lsb.hide("tmp.png", steg_key)
+	secret.save("tmp.png")
+	secret_frame = cv2.imread("tmp.png")
+	frame_array[n//2] = secret_frame
 	path3 = _wrap_up(path1, frame_array, size)
 	tmpvid = cv2.VideoCapture(path3)
 	frame_array, size = _split_frames(tmpvid)
-	print("now")
-	print(_lsb_decode(frame_array[0]))
 	return path3
 
 def steg_video(path1, path2):
@@ -41,5 +43,3 @@ def steg_video(path1, path2):
 	frame_array1[0] = _lsb_encode(frame_array1[0], steg_key)
 	path3 = _wrap_up(path1, frame_array1, size1) 
 	return path3
-
-steg_photo("/home/osboxes/secret-frames/video.mp4", "/home/osboxes/secret-frames/Lena.jpg")
